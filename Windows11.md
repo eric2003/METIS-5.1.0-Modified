ee9c3edae1784d5dae8abf3cae68b0b52aa67d93
eric <fantasy_2003_@hotmail.com>
2022-05-24 13:22:00 +0800

    modify metis.h.in

--- Windows11.md
+++ Windows11.md
@@ -1,4 +1,6 @@
 cd d:\work\metis_work\METIS-5.1.0-Modified\build\
 
+cmake -DSHARED=TRUE  ..
+
 $env:path+="d:/work/metis_work/METIS-5.1.0-Modified/build/libmetis/Debug/;"
 & ./METIS.sln
--- include/metis.h
+++ include/metis.h
@@ -10,7 +10,27 @@
 #ifndef _METIS_H_
 #define _METIS_H_ 
 
-#include "metis_def.h"
+//#include "metis_def.h"
+#ifndef METIS_DLL
+# ifdef _WIN32
+#  if defined( BUILD_DLL )
+#    define METIS_DLL __declspec(dllexport)
+#  elif defined( USE_DLL )
+#    define METIS_DLL __declspec(dllimport)
+#  else
+#    define METIS_DLL
+#  endif
+# else
+#  define METIS_DLL
+# endif
+#endif
+
+
+#if defined(__cdecl)
+#define METIS_API(type) METIS_DLL type __cdecl
+#else
+#define METIS_API(type) METIS_DLL type
+#endif
 
 /****************************************************************************
 * A set of defines that can be modified by the user
@@ -176,15 +196,6 @@ typedef __int64 int64_t;
 /*------------------------------------------------------------------------
 * Function prototypes 
 *-------------------------------------------------------------------------*/
-
-//#ifdef _WINDLL
-//#define METIS_API(type) __declspec(dllexport) type __cdecl
-//#elif defined(__cdecl)
-//#define METIS_API(type) type __cdecl
-//#else
-//#define METIS_API(type) type
-//#endif
-
 
 
 #ifdef __cplusplus
